43b0e10c9b1bfd68d04f1309550fa47f126774e7
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2024-09-23 13:17:22 -0400

    steps and pyramid problems

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Iterative solution:
+
+// function pyramid(n) {
+//     // calculate midpoint of pyramid's base
+//     const mid = Math.floor((2 * n - 1) / 2);
+//     // iterate over rows
+//     for (let i = 0; i < n; i++) {
+//         // str holds characters for current level
+//         let str = "";
+//         // iterate over columns
+//         for (let j = 0; j < 2 * n - 1; j++) {
+//             // if column is within the range of the pyramid's base
+//             if (mid - i <= j && mid + i >= j) {
+//                 // add a "#" to str
+//                 str += "#";
+//             } else {
+//                 // add a space to str
+//                 str += " ";
+//             }
+//         }
+//         // log str
+//         console.log(str);
+//     }
+// }
+
+// Recursive solution:
+
+function pyramid(n, row=0, str=''){
+    if(n===row){
+        return;
+    }
+    if(2*n-1 === str.length){
+        console.log(str);
+        return pyramid(n, row+1);
+    }
+    const mid = Math.floor((2 * n - 1) / 2);
+    const add = mid - row <= str.length && mid + row >= str.length ? '#' : ' ';
+    pyramid(n, row, str + add);
+}
 
 module.exports = pyramid;
--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,35 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+	for (let i = 0; i < n; i++) {
+		let str = "";
+		for (let j = 0; j < n; j++) {
+			if (j <= i) {
+				str += "#";
+			} else {
+				str += " ";
+			}
+		}
+		console.log(str);
+	}
+}
+
+// Recursive solution:
+// function steps(n, row = 0, stair = "") {
+// base case of n === 0
+// 	if (n === row) {
+// 		return;
+// 	}
+//  recursive call logging stair and iterating the row we are on
+// 	if (n === stair.length) {
+// 		console.log(stair);
+// 		return steps(n, row + 1);
+// 	}
+// 	conditional to add "#" or " " to the stair
+// 	const add = stair.length <= row ? "#" : " ";
+// 	steps(n, row, stair + add);
+// }
+
 
 module.exports = steps;
