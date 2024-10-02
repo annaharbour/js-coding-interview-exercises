2191f43694a65c33209c68612774799143b710b8
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2024-10-02 14:01:56 -0400

    validate, tree, mergesort, bubblesort, selection sort, level width, binary search tree, js events

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,64 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				// assigning the lesser value to a temporary variable
+				const temp = arr[j + 1];
+				// setting the lesser value to the previous index
+				arr[j + 1] = arr[j];
+				// setting the greater index to the temporary value
+				arr[j] = temp;
+			}
+		}
+	}
+	return arr;
 }
 
+// The "prove me wrong" algo
 function selectionSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let min = i;
 
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+
+		if (min !== i) {
+			let temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+	}
+	return arr;
 }
 
 function mergeSort(arr) {
+	if (arr.length === 1) {
+		return arr;
+	}
 
+	const mid = Math.floor(arr.length / 2);
+	const left = arr.slice(0, mid);
+	const right = arr.slice(mid);
+
+	return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+	const result = [];
 
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+	return [...result, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
